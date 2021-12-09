4e424a65620c1cb38e4696cc13ae7673a1fe97dd
ino012345 <yutainoue0225@gmail.com>
2021-12-09 10:02:03 +0900

    2021-12-09

--- WordPressTheme/front-page.php
+++ WordPressTheme/front-page.php
@@ -1,5 +1,274 @@
 <?php get_header(); ?>
 
-
+<section class="fv">
+  <ul class="fv__imageList">
+    <li class="fv__image">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/top.jpg" alt="イメージ画像">
+    </li>
+    <li class="fv__image">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/top.jpg" alt="イメージ画像">
+    </li>
+    <li class="fv__image">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/top.jpg" alt="イメージ画像">
+    </li>
+    <li class="fv__image">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/top.jpg" alt="イメージ画像">
+    </li>
+  </ul>
+</section>
+<section class="news">
+  <h1 class="section-heading mini">おしらせ</h1>
+  <ul class="news__list">
+    <li class="news__item">
+      <p class="news__date">2021.12.01</p>
+      <a href="#" class="news__title">オフィシャルメンバーに「ようこ」が追加されました！</a>
+    </li>
+    <li class="news__item">
+      <p class="news__date">2021.12.01</p>
+      <a href="#" class="news__title">夏季休暇のご案内</a>
+    </li>
+    <li class="news__item">
+      <p class="news__date">2021.12.01</p>
+      <a href="#" class="news__title">オープンキャンペーンのおしらせ</a>
+    </li>
+  </ul>
+  <div class="news__bannerArea">
+    <figure class="news__banner">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/noimage.jpg" alt="バナー">
+    </figure>
+    <figure class="news__banner">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/noimage.jpg" alt="バナー">
+    </figure>
+  </div>
+  <div class="btn__wrap">
+    <a href="#" class="btn">すべてのおしらせをチェック</a>
+  </div>
+</section>
+<section class="member">
+  <h1 class="section-heading">オフィシャルメンバー</h1>
+  <ul class="member__list">
+    <li class="member__item">
+      <a href="#" class="member__link">
+        <figure class="member__image">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon.png" alt="メンバーの紹介画像">
+        </figure>
+        <p class="member__name">ようこ</p>
+        <p class="member__from">神奈川県</p>
+        <p class="member__favorite">泳がせ釣り</p>
+        <p class="member__arrow">PROFILE >>></p>
+      </a>
+    </li>
+    <li class="member__item">
+      <a href="#" class="member__link">
+        <figure class="member__image">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon.png" alt="メンバーの紹介画像">
+        </figure>
+        <p class="member__name">ようこ</p>
+        <p class="member__from">神奈川県</p>
+        <p class="member__favorite">泳がせ釣り</p>
+        <p class="member__arrow">PROFILE >>></p>
+      </a>
+    </li>
+    <li class="member__item">
+      <a href="#" class="member__link">
+        <figure class="member__image">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon.png" alt="メンバーの紹介画像">
+        </figure>
+        <p class="member__name">ようこ</p>
+        <p class="member__from">神奈川県</p>
+        <p class="member__favorite">泳がせ釣り</p>
+        <p class="member__arrow">PROFILE >>></p>
+      </a>
+    </li>
+    <li class="member__item">
+      <a href="#" class="member__link">
+        <figure class="member__image">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon.png" alt="メンバーの紹介画像">
+        </figure>
+        <p class="member__name">ようこ</p>
+        <p class="member__from">神奈川県</p>
+        <p class="member__favorite">泳がせ釣り</p>
+        <p class="member__arrow">PROFILE >>></p>
+      </a>
+    </li>
+    <li class="member__item">
+      <a href="#" class="member__link">
+        <figure class="member__image">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon.png" alt="メンバーの紹介画像">
+        </figure>
+        <p class="member__name">ようこ</p>
+        <p class="member__from">神奈川県</p>
+        <p class="member__favorite">泳がせ釣り</p>
+        <p class="member__arrow">PROFILE >>></p>
+      </a>
+    </li>
+    <li class="member__item">
+      <a href="#" class="member__link">
+        <figure class="member__image">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon.png" alt="メンバーの紹介画像">
+        </figure>
+        <p class="member__name">ようこ</p>
+        <p class="member__from">神奈川県</p>
+        <p class="member__favorite">泳がせ釣り</p>
+        <p class="member__arrow">PROFILE >>></p>
+      </a>
+    </li>
+    <li class="member__item">
+      <a href="#" class="member__link">
+        <figure class="member__image">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon.png" alt="メンバーの紹介画像">
+        </figure>
+        <p class="member__name">ようこ</p>
+        <p class="member__from">神奈川県</p>
+        <p class="member__favorite">泳がせ釣り</p>
+        <p class="member__arrow">PROFILE >>></p>
+      </a>
+    </li>
+    <li class="member__item">
+      <a href="#" class="member__link">
+        <figure class="member__image">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon.png" alt="メンバーの紹介画像">
+        </figure>
+        <p class="member__name">ようこ</p>
+        <p class="member__from">神奈川県</p>
+        <p class="member__favorite">泳がせ釣り</p>
+        <p class="member__arrow">PROFILE >>></p>
+      </a>
+    </li>
+    <li class="member__item">
+      <a href="#" class="member__link">
+        <figure class="member__image">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon.png" alt="メンバーの紹介画像">
+        </figure>
+        <p class="member__name">ようこ</p>
+        <p class="member__from">神奈川県</p>
+        <p class="member__favorite">泳がせ釣り</p>
+        <p class="member__arrow">PROFILE >>></p>
+      </a>
+    </li>
+    <li class="member__item">
+      <a href="#" class="member__link">
+        <figure class="member__image">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon.png" alt="メンバーの紹介画像">
+        </figure>
+        <p class="member__name">ようこ</p>
+        <p class="member__from">神奈川県</p>
+        <p class="member__favorite">泳がせ釣り</p>
+        <p class="member__arrow">PROFILE >>></p>
+      </a>
+    </li>
+    <li class="member__item">
+      <a href="#" class="member__link">
+        <figure class="member__image">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon.png" alt="メンバーの紹介画像">
+        </figure>
+        <p class="member__name">ようこ</p>
+        <p class="member__from">神奈川県</p>
+        <p class="member__favorite">泳がせ釣り</p>
+        <p class="member__arrow">PROFILE >>></p>
+      </a>
+    </li>
+    <li class="member__item">
+      <a href="#" class="member__link">
+        <figure class="member__image">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon.png" alt="メンバーの紹介画像">
+        </figure>
+        <p class="member__name">ようこ</p>
+        <p class="member__from">神奈川県</p>
+        <p class="member__favorite">泳がせ釣り</p>
+        <p class="member__arrow">PROFILE >>></p>
+      </a>
+    </li>
+  </ul>
+  <div class="btn__wrap">
+    <a href="#" class="btn">他のオフィシャルメンバーをチェック</a>
+  </div>
+</section>
+<section class="blog">
+  <h1 class="section-heading">メンバーズブログ</h1>
+  <ul class="blog__list">
+    <li class="blog__item">
+      <figure class="blog__image">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/blog1.png" alt="アイキャッチ">
+      </figure>
+      <p class="blog__date">2021.12.01</p>
+      <a href="#" class="blog__title">記事のタイトル。ここをクリックすると記事が見れるようにします。</a>
+      <a href="#" class="blog__author">
+        <figure class="blog__icon">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon.png" alt="メンバーのアイコン">
+        </figure>
+        <p class="blog__name">ようこ</p>
+      </a>
+    </li>
+    <li class="blog__item">
+      <figure class="blog__image">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/blog2.png" alt="アイキャッチ">
+      </figure>
+      <p class="blog__date">2021.12.01</p>
+      <a href="#" class="blog__title">記事のタイトル。ここをクリックすると記事が見れるようにします。</a>
+      <a href="#" class="blog__author">
+        <figure class="blog__icon">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon.png" alt="メンバーのアイコン">
+        </figure>
+        <p class="blog__name">ようこ</p>
+      </a>
+    </li>
+    <li class="blog__item">
+      <figure class="blog__image">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/blog1.png" alt="アイキャッチ">
+      </figure>
+      <p class="blog__date">2021.12.01</p>
+      <a href="#" class="blog__title">記事のタイトル。ここをクリックすると記事が見れるようにします。</a>
+      <a href="#" class="blog__author">
+        <figure class="blog__icon">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon.png" alt="メンバーのアイコン">
+        </figure>
+        <p class="blog__name">ようこ</p>
+      </a>
+    </li>
+    <li class="blog__item">
+      <figure class="blog__image">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/blog2.png" alt="アイキャッチ">
+      </figure>
+      <p class="blog__date">2021.12.01</p>
+      <a href="#" class="blog__title">記事のタイトル。ここをクリックすると記事が見れるようにします。</a>
+      <a href="#" class="blog__author">
+        <figure class="blog__icon">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon.png" alt="メンバーのアイコン">
+        </figure>
+        <p class="blog__name">ようこ</p>
+      </a>
+    </li>
+    <li class="blog__item">
+      <figure class="blog__image">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/blog1.png" alt="アイキャッチ">
+      </figure>
+      <p class="blog__date">2021.12.01</p>
+      <a href="#" class="blog__title">記事のタイトル。ここをクリックすると記事が見れるようにします。</a>
+      <a href="#" class="blog__author">
+        <figure class="blog__icon">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon.png" alt="メンバーのアイコン">
+        </figure>
+        <p class="blog__name">ようこ</p>
+      </a>
+    </li>
+    <li class="blog__item">
+      <figure class="blog__image">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/blog2.png" alt="アイキャッチ">
+      </figure>
+      <p class="blog__date">2021.12.01</p>
+      <a href="#" class="blog__title">記事のタイトル。ここをクリックすると記事が見れるようにします。</a>
+      <a href="#" class="blog__author">
+        <figure class="blog__icon">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon.png" alt="メンバーのアイコン">
+        </figure>
+        <p class="blog__name">ようこ</p>
+      </a>
+    </li>
+  </ul>
+  <div class="btn__wrap">
+    <a href="#" class="btn">すべてのブログをチェック</a>
+  </div>
+</section>
 
 <?php get_footer(); ?>
