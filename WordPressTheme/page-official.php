7561b862f468df2ca11213182f9105a0c5978233
ino012345 <yutainoue0225@gmail.com>
2021-12-29 06:33:14 +0900

    2021-12-29

--- WordPressTheme/page-official.php
+++ WordPressTheme/page-official.php
@@ -1,49 +1,34 @@
 <?php get_header(); ?>
 
 <section class="recruit-title">
-  <div class="recruit-title__left">
-    <figure class="recruit-title__image image1">
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/official1.jpg" alt="釣りガールイメージ画像">
-    </figure>
-    <figure class="recruit-title__image image2">
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/official2.jpg" alt="釣りガールイメージ画像">
-    </figure>
-    <figure class="recruit-title__image image3">
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/official3.jpg" alt="釣りガールイメージ画像">
-    </figure>    
-  </div>
   <div class="recruit-title__body">
     <figure class="recruit-title__logo">
       <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo-white.svg" alt="ロゴ">
     </figure>
     <h1>オフィシャルメンバー募集</h1>
   </div>
-  <div class="recruit-title__right">
-    <figure class="recruit-title__image image4">
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/official4.jpg" alt="釣りガールイメージ画像">
-    </figure>
-    <figure class="recruit-title__image image5">
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/official5.jpg" alt="釣りガールイメージ画像">
-    </figure>
-    <figure class="recruit-title__image image6">
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/official6.jpg" alt="釣りガールイメージ画像">
-    </figure>
-  </div>
 </section>
-<div class="recruit__cta">
+<section class="recruit__cta">
+  <p class="recruit__catch">ご登録は<span>無料</span></p>
   <a href="#" class="btn block official">今すぐ応募する</a>
   <div class="recruit__explainWrap">
     <p class="recruit__explain">※オフィシャルメンバーは無料でご登録できます。</p>
     <p class="recruit__explain">※LINE友達追加後に応募フォームが届きます。</p>
   </div>
-</div>
+</section>
 <section class="recruit">
   <div class="recruit__overview">
-    <h2 class="recruit__heading">#オフィシャルメンバー募集中</h2>
+    <h2 class="recruit__heading white"><span class="left"></span><p>#オフィシャルメンバー募集中</p><span class="right"></span></h2>
     <p class="recruit__overviewText">釣り美女のオフィシャルメンバーは<br>"発信力を高めたい"<br>"釣りを通じてお仕事がしたい"<br>そんな釣りガールを応援するための<br>メンバーシップ制度です。（無料）</p>
+    <figure class="recruit__overviewImage left">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/b0812.png" alt="魚を水揚げする女性">
+    </figure>
+    <figure class="recruit__overviewImage right">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/b0680.png" alt="写真を撮る女性">
+    </figure>
   </div>
   <div class="recruit__recommend">
-    <h2 class="recruit__heading">こんな方にオススメです！</h2>
+    <h2 class="recruit__heading white"><span class="left"></span><p>こんな方にオススメです！</p><span class="right"></span></h2>
     <div class="recruit__media">
       <figure class="recruit__mediaImage">
         <img src="<?php echo get_template_directory_uri(); ?>/assets/img/official1.jpg" alt="釣り美女の画像">
@@ -56,36 +41,43 @@
       </div>
     </div>
   </div>
-  <div class="recruit__service">
-    <h2 class="recruit__heading">オフィシャルメンバーに提供されるサービス</h2>
-    <div class="recruit__cardUnit">
-      <div class="recruit__card">
-        <p class="recruit__cardHead">インフルエンサー<br>案件の紹介</p>
-        <figure class="recruit__cardIcon">
-          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/b0954.png" alt="メディア取材のイメージ">
-        </figure>
-        <p class="recruit__cardExplain">企業様から依頼のあった案件を<br>オフィシャルメンバーにご紹介します。<br>TV出演、メディア出演・取材、<br>商品PR等々、あなたのインフルエンス力を報酬に変えることができます。</p>
-      </div>
-      <div class="recruit__card">
-        <p class="recruit__cardHead">釣り美女WEBに掲載<br>&システムの利用</p>
-        <figure class="recruit__cardIcon">
-          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/b0749.png" alt="システムのイメージ">
-        </figure>
-        <p class="recruit__cardExplain">Instagramフォロワー1万超え、月間300万インプレッションを誇る釣り美女のWEBサイトににオフィシャルメンバーとして掲載。さらにプロフィールページとブログ機能をご利用頂くことができます。</p>
-      </div>
-    </div>
-    <p class="recruit__serviceText">オフィシャルメンバーには上記のサービスを<br><span>全て無料</span>（0円）でご提供させていただきます。</p>
-  </div>
 </section>
-<div class="recruit__cta">
+<section class="recruit__service">
+  <h2 class="recruit__heading"><span class="left"></span><p>オフィシャルメンバーに提供されるサービス</p><span class="right"></span></h2>
+  <div class="recruit__cardUnit">
+    <div class="recruit__card">
+      <p class="recruit__cardHead">インフルエンサー<br>案件の紹介</p>
+      <figure class="recruit__cardIcon">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/b0954.png" alt="メディア取材のイメージ">
+      </figure>
+      <p class="recruit__cardExplain">企業様から依頼のあった案件を<br>オフィシャルメンバーにご紹介します。<br>TV出演、メディア出演・取材、<br>商品PR等々、あなたのインフルエンス力を報酬に変えることができます。</p>
+    </div>
+    <div class="recruit__card">
+      <p class="recruit__cardHead">釣り美女WEBに掲載<br>&システムの利用</p>
+      <figure class="recruit__cardIcon">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/b0749.png" alt="システムのイメージ">
+      </figure>
+      <p class="recruit__cardExplain">Instagramフォロワー1万超え、月間300万インプレッションを誇る釣り美女のWEBサイトににオフィシャルメンバーとして掲載。さらにプロフィールページとブログ機能をご利用頂くことができます。</p>
+    </div>
+  </div>
+  <p class="recruit__serviceText">オフィシャルメンバーには上記のサービスを<br><span>全て無料</span>（0円）でご提供させていただきます。</p>
+  <figure class="recruit__serviceBackImage left">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/asari-1.svg" alt="貝殻の画像">
+  </figure>
+  <figure class="recruit__serviceBackImage right">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/asari-2.svg" alt="貝殻の画像">
+  </figure>
+</section>
+<section class="recruit__cta">
+  <p class="recruit__catch">ご登録は<span>無料</span></p>
   <a href="#" class="btn block official">今すぐ応募する</a>
   <div class="recruit__explainWrap">
     <p class="recruit__explain">※オフィシャルメンバーは無料でご登録できます。</p>
     <p class="recruit__explain">※LINE友達追加後に応募フォームが届きます。</p>
   </div>
-</div>
+</section>
 <section class="application">
-  <h2 class="recruit__heading">募集要項</h2>
+  <h2 class="recruit__heading"><span class="left"></span><p>募集要項</p><span class="right"></span></h2>
   <ul class="application__list">
     <li class="application__item">
       <p class="application__head">釣りスキル</p>
@@ -117,9 +109,15 @@
     </li>
   </ul>
   <p class="application__text">釣り美女では既に大成している釣りガールよりも<br>これから一緒に成長していける釣りガールを大歓迎しています。<br><br>一緒に成長できる環境を提供できるよう努めていきます。</p>
+  <figure class="application__backImage left">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/leaf-1.svg" alt="海藻の画像">
+  </figure>
+  <figure class="application__backImage right">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/leaf-2.svg" alt="海藻の画像">
+  </figure>
 </section>
 <section class="flow">
-  <h2 class="recruit__heading">お申し込みの流れ</h2>
+  <h2 class="recruit__heading"><span class="left"></span><p>お申し込みの流れ</p><span class="right"></span></h2>
   <ul class="flow__list">
     <li class="flow__item">
       <div class="flow__itemInner">
@@ -146,27 +144,31 @@
       </div>
     </li>
   </ul>
+  <figure class="flow__backImage">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/fish-2.svg" alt="魚の群れの画像">
+  </figure>
 </section>
-<div class="recruit__cta">
+<section class="recruit__cta">
+  <p class="recruit__catch">ご登録は<span>無料</span></p>
   <a href="#" class="btn block official">今すぐ応募する</a>
   <div class="recruit__explainWrap">
     <p class="recruit__explain">※オフィシャルメンバーは無料でご登録できます。</p>
     <p class="recruit__explain">※LINE友達追加後に応募フォームが届きます。</p>
   </div>
-</div>
+</section>
 <section class="message">
-  <h2 class="recruit__heading">運営からのメッセージ</h2>
+  <h2 class="recruit__heading"><span class="left"></span><p>運営からのメッセージ</p><span class="right"></span></h2>
   <div class="message__main">
     <p class="message__text">いつもお世話になっております。<br><br>Instagramアカウントを開設して、<br>わずか1年で1万フォローを獲得し、<br>月間300インプレッションを獲得できるまで<br>成長することができました。<br><br>これも釣りガールの皆様のおかげです。<br>ありがとうございます。<br><br>この恩をどうすれば返せるか考え<br>この度釣り美女WEBをOPENしました。<br><br>釣り美女WEBは<br>Instagramという枠を超えて、<br>さらに釣りガールの皆さんの露出を増やし、<br>影響力を高めることができる仕組みを用意しました。<br><br>まず一つ目はブログ機能です。<br><br>今更ブログ？と思われるかもしれませんが<br>InstagramやTwitterが流行っている今だからこそ、<br>ブログが狙い目です。<br><br>釣りガールでブログ書いてる人って<br>そこまで多くないですよね？<br><br>つまり今ブログに取り組むと、<br>他の釣りガールと差別化を図ることができます。<br><br>また、ブログはGoogleやYahoo検索でヒットしますので<br>Instagramではアプローチできなかった層にあなたを知ってもらう事ができます。<br><br>2つ目の機能がプロフィールページです。<br><br>InstagramからURLを一つだけ貼ることができますが、そこに是非釣り美女WEBで作成したプロフィールページを設定してみてください。<br><br>釣り美女WEBで作成したプロフィールページでは<br>・プロフィール<br>・プロフィール写真<br>・各種SNSへのリンク<br>・ブログ一覧<br><br>をキレイに表示できるようにしました。<br><br>Instagramプロフィール<br>↓<br>釣り美女WEBプロフィール<br><br>へと誘導することで、よりあなたの事を知ってもらえ、ファン化、フォロワー増加を促進します。<br><br>この他にも今後、<br>様々なサービスを展開予定ですので、<br><br>釣りガールの皆さんは是非、<br>釣り美女WEBにご登録いただければと思います。<br><br>それでは、今後とも宜しくおねがいします。</p>
   </div>
 </section>
 <section class="question">
-  <h2 class="recruit__heading">よくあるご質問</h2>
+  <h2 class="recruit__heading white"><span class="left"></span><p>よくあるご質問</p><span class="right"></span></h2>
   <ul class="accordion-area">
     
     <li><!--li ~ /li までが一つのアコーディオン-->
     <section>
-      <h3 class="title">私でも大丈夫ですか？</h3>
+      <h3 class="title"><span>Q.</span>私でも大丈夫ですか？</h3>
       <div class="box">
         <p>審査はフォロワー数ではなく、人柄や釣りに対する姿勢を見て判断させていただいております。<br>有名、無名も判断材料にはなりません。<br>共に成長し、釣り美女を盛り上げてくれる方は大歓迎します。<br>一緒に成長していきましょう！</p>
       </div>
@@ -174,7 +176,7 @@
   </li>
   <li>
   <section>
-  <h3 class="title">どんなお仕事がありますか？</h3>
+  <h3 class="title"><span>Q.</span>どんなお仕事がありますか？</h3>
   <div class="box">
   <p>一番多いのは商品PRです。商品をお送りし実際にご利用いただき、投稿して頂くお仕事です。<br>またメディアの釣りガール特集や、アウトドアブランドの新商品のモニターなど様々な案件がございます。</p>
   </div>
@@ -182,7 +184,7 @@
   </li>
   <li>
   <section>
-  <h3 class="title">紹介を断ることもできますか？</h3>
+  <h3 class="title"><span>Q.</span>紹介を断ることもできますか？</h3>
   <div class="box">
   <p>はい。もちろん、この商品を紹介するのは嫌だな...。と感じた場合はお断り頂いて構いません。</p>
   </div>
@@ -190,7 +192,7 @@
   </li>
   <li>
   <section>
-  <h3 class="title">報酬はどのくらいもらえますか？</h3>
+  <h3 class="title"><span>Q.</span>報酬はどのくらいもらえますか？</h3>
   <div class="box">
   <p>報酬に関してはクライアントさんの都合もあり、フォロワー数や案件によって異なりますが、数千円～数万円の案件が多いです。中には数十万円の案件もございます。</p>
   </div>
@@ -198,7 +200,7 @@
   </li>
   <li>
   <section>
-  <h3 class="title">釣り以外の案件もありますか？</h3>
+  <h3 class="title"><span>Q.</span>釣り以外の案件もありますか？</h3>
   <div class="box">
   <p>はい、あります。釣り以外だと料理、アウトドア、アパレル、美容の案件が多いです。</p>
   </div>
@@ -206,7 +208,7 @@
   </li>
   <li>
   <section>
-  <h3 class="title">ブログはどのくらいの頻度で更新すればいいですか？</h3>
+  <h3 class="title"><span>Q.</span>ブログはどのくらいの頻度で更新すればいいですか？</h3>
   <div class="box">
   <p>とくに指定はありませんが、最低月1で更新してもらえると嬉しいです。</p>
   </div>
@@ -214,7 +216,7 @@
   </li>
   <li>
   <section>
-  <h3 class="title">応募から何日くらいで審査の結果は届きますか？</h3>
+  <h3 class="title"><span>Q.</span>応募から何日くらいで審査の結果は届きますか？</h3>
   <div class="box">
   <p>3営業日以内には返信をするようにしております。</p>
   </div>
@@ -222,7 +224,7 @@
   </li>
   <li>
   <section>
-  <h3 class="title">どんな人が審査に受かりやすいですか？</h3>
+  <h3 class="title"><span>Q.</span>どんな人が審査に受かりやすいですか？</h3>
   <div class="box">
   <p>釣りの投稿が多い、写真がキレイ、釣果が凄い、初心者釣りガールの参考になる、文章が読みやすい、見ていてポジティブになれる投稿が多い方は審査に通りやすいです。</p>
   </div>
@@ -230,7 +232,7 @@
   </li>
   <li>
   <section>
-  <h3 class="title">ブログで個人的な商品のPRをしても大丈夫ですか？</h3>
+  <h3 class="title"><span>Q.</span>ブログで個人的な商品のPRをしても大丈夫ですか？</h3>
   <div class="box">
   <p>はい、ブログは自由にお使いいただいて大丈夫ですが、あまりに釣りに関係のない投稿が続いたりするとお声がけさせて頂く場合があります。</p>
   </div>
@@ -238,13 +240,20 @@
   </li>
 
   </ul>
+  <figure class="question__backImage left">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/sango-2.svg" alt="サンゴの画像">
+  </figure>
+  <figure class="question__backImage right">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/sango-1.svg" alt="サンゴの画像">
+  </figure>
 </section>
-<div class="recruit__cta">
+<section class="recruit__cta">
+  <p class="recruit__catch">ご登録は<span>無料</span></p>
   <a href="#" class="btn block official">今すぐ応募する</a>
   <div class="recruit__explainWrap">
     <p class="recruit__explain">※オフィシャルメンバーは無料でご登録できます。</p>
     <p class="recruit__explain">※LINE友達追加後に応募フォームが届きます。</p>
   </div>
-</div>
+</section>
 
 <?php get_footer(); ?>
